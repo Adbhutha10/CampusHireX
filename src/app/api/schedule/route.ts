1355fb8f4469ccb56448a72bf3cc6caf1ba211e0
Adbhutha10 <23211a0533@bvrit.ac.in>
2026-03-27 20:29:09 +0530

    fix: add descriptions to @ts-expect-error directives

--- src/app/api/schedule/route.ts
+++ src/app/api/schedule/route.ts
@@ -61,7 +61,7 @@ export async function POST(req: Request) {
   } catch (err: unknown) {
     if (err instanceof Error) {
       if (err.name === "ZodError") {
-        // @ts-expect-error
+        // @ts-expect-error: ZodError contains errors property but type is unknown
         return new NextResponse(err.errors[0].message, { status: 400 })
       }
       return new NextResponse(err.message, { status: 500 })
